88e6bc925cf963003a42c33652e0d684631eb198
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2020-02-27 20:21:55 -0500

    set up Home

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,5 +1,68 @@
 <template>
-  <div id="app">
-    <h2>searchhhh</h2> 
-  </div>
+	<b-container class="bv-example-row">
+		<b-row class="justify-content-md-center">
+			<b-col col lg="2"></b-col>
+
+			<b-col cols="12" md="auto">
+				<b-form @submit="onSubmit" @reset="onReset" v-if="show">
+					<b-form-group
+					id="input-group-1"
+					label="Engrese el dominio:"
+					label-for="input-1"
+					>
+					<b-form-input
+					id="input-1"
+					v-model="form.email"
+					type="email"
+					required
+					placeholder="Ingrese el dominio"
+					></b-form-input>
+				</b-form-group>
+
+				<b-button type="submit" variant="primary">Submit</b-button>
+				<b-button type="reset" variant="danger">Reset</b-button>
+			</b-form>
+			<b-card class="mt-3" header="Form Data Result">
+				<pre class="m-0">{{ form }}</pre>
+			</b-card>
+		</b-col>
+		<b-col col lg="2"></b-col>
+	</b-row>
+</b-container>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			form: {
+				email: '',
+				name: '',
+				food: null,
+				checked: []
+			},
+			foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+			show: true
+		}
+	},
+	methods: {
+		onSubmit(evt) {
+			evt.preventDefault()
+			alert(JSON.stringify(this.form))
+		},
+		onReset(evt) {
+			evt.preventDefault()
+        // Reset our form values
+        this.form.email = ''
+        this.form.name = ''
+        this.form.food = null
+        this.form.checked = []
+        // Trick to reset/clear native browser form validation state
+        this.show = false
+        this.$nextTick(() => {
+        	this.show = true
+        })
+    }
+}
+}
+</script>
